e096db7ce8b09799fdf6edec701f994007994d77
Krzysztof Bera <krzysztof.bera9@gmail.com>
2021-07-06 14:14:30 +0200

    visulalization of data works

--- receiver/uart.py
+++ receiver/uart.py
@@ -1,4 +1,10 @@
 import serial
+import datetime
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib
+matplotlib.use("tkAgg")
 
 
 def get_number(data):
@@ -15,11 +21,32 @@ def fetch_data():
 # open UART connection
 ser = serial.Serial('COM3', baudrate=115200)
 
+# initialize plot
+plot_window = 20
+y_var = np.array(np.zeros([plot_window]))
+
+plt.ion()
+fig, ax = plt.subplots()
+line, = ax.plot(y_var)
+
 while True:
     try:
         temp, hum, press = fetch_data()
-        temperature = get_number(temp)
-        print(temperature)
-    except KeyboardInterrupt:
-        print("Keyboard Interrupt")
+        try:
+            temperature = get_number(temp)
+        except:
+            continue
+        with open("test_data.csv", "a") as f:
+            writer = csv.writer(f, delimiter=',')
+            writer.writerow([datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), temperature])
+        y_var = np.append(y_var, temperature)
+        y_var = y_var[1:plot_window+1]
+        line.set_ydata(y_var)
+        ax.relim()
+        ax.autoscale_view()
+        fig.canvas.draw()
+        fig.canvas.flush_events()
+    except Exception as e:
+        print(e)
         break
+    
