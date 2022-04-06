572603f0b660e8e4b1ca4b7e3b904ddfc29d303c
Krzysztof Bera <krzysztof.bera9@gmail.com>
2022-04-06 21:41:00 +0200

    - add pressure and humidity buttons and labels - add docstrings functions and class of the project

--- python_app/controller.py
+++ python_app/controller.py
@@ -3,12 +3,30 @@ from app_gui import AppGui
 
 
 class Controller:
+    """ Class responsible for performing requests """
 
     def __init__(self, view: AppGui):
+        """
+        Set reference to GUI class and init UART connection
+        :param view: reference to the GUI of the application
+        """
         self.view = view
         self.uart_handler = UartHandler()
 
     def handle_request_temperature(self):
+        """ Action performed when requesting last temperature measurement """
         self.uart_handler.send_command("get temp")
         result = self.uart_handler.read_last_data()
         self.view.temperature_label.setText(f"{result} deg C")
+
+    def handle_request_humidity(self):
+        """ Action performed when requesting last humidity measurement """
+        self.uart_handler.send_command("get hum")
+        result = self.uart_handler.read_last_data()
+        self.view.humidity_label.setText(f"{result} %")
+
+    def handle_request_pressure(self):
+        """ Action performed when requesting last pressure measurement """
+        self.uart_handler.send_command("get press")
+        result = self.uart_handler.read_last_data()
+        self.view.pressure_label.setText(f"{result} hPa")
--- python_app/uart_handler.py
+++ python_app/uart_handler.py
@@ -2,7 +2,7 @@ import serial
 
 
 class UartHandler(serial.Serial):
-    """ Wrapper around serial.Serial class """
+    """ Class responsible for UART connection """
 
     def __init__(self):
         """ Init connection with UART """
@@ -11,7 +11,15 @@ class UartHandler(serial.Serial):
                          stopbits=serial.STOPBITS_ONE)
 
     def send_command(self, command: str) -> None:
+        """
+        Write a command to UART
+        :param command - command to be sent to UART (without '\n' or '\r' chars)
+        """
         self.write(f"{command}\r\n".encode())
 
     def read_last_data(self) -> str:
+        """
+        Read last line put on UART
+        :return: last line put on UART in the form of string
+        """
         return self.readline().decode().strip()
